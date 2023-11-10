07db8183028951c063adafb35c9eff53d11958d9
yyyjynbdhycx <147242900+yyyjynbdhycx@users.noreply.github.com>
2023-11-10 15:01:04 +0800

    Update 高尔顿板矢量法.py

--- 高尔顿板矢量法.py
+++ 高尔顿板矢量法.py
@@ -4,29 +4,7 @@ from matplotlib.lines import Line2D
 import numpy as np
 import random
 
-radius = 0.6  # 小球半径
-ballnum = 100  # 小球数量
-g = -9.81  # m/s^2
-dt = 0.02  # 取的小时间微元
-# 设置xy轴范围
-xmin = -32
-ymin = -44
-ymax = 32
-xmax = 32
-# 定义高尔顿板钉子的参数
-rnail = 0.2  # 钉子的半径
-dx = 4.8  # 横向间距
-dy = 2.5  # 纵向间距
-cn = 12  # 层数
-# 定义侧板位置
-sx, sy = 2, 33
-# 定义各碰撞系数
-factor = 0.1  # 与边界的碰撞系数
-railfactor = 0.8  # 与钉子碰撞的碰撞系数
-funnelfactor = 0.8  # 与漏斗碰撞的碰撞系数
-parfactor = 0.5  # 与下方隔板的碰撞系数
-k1 = 0.98  # 两小球碰撞因子
-nailnum = int(cn * (cn - 1) / 2)  # 钉子总数
+
 
 
 class Ball:
@@ -44,11 +22,15 @@ class Nail:
         self.r = np.array(r)
 
 
+# 获取特定向量的垂直向量
 def getvertical_vector(vec):
     return np.array([vec[1], -vec[0]])
 
 
+# 自由落体
 def freefall(ball):
+    g = -9.81  # m/s^2
+    dt = 0.01  # 取的小时间微元
     tb = ball
     for i in range(ballnum):
         tb[i].r = ball[i].r + ball[i].v * dt
@@ -57,8 +39,10 @@ def freefall(ball):
     return tb
 
 
+# 碰撞边界
 def bouncewall(ball):
     tb = ball
+    factor = 0.2  # 与边界的碰撞系数
     for i in range(ballnum):
         if ball[i].r[0] - Ball.radius < xmin:
             tb[i].r[0] = xmin + Ball.radius
@@ -78,76 +62,87 @@ def bouncewall(ball):
 # 定义碰撞上方漏斗
 def bouncefunnel(ball):
     b1 = ball
+    radius = Ball.radius
+    funnelfactor = 0.8  # 与漏斗碰撞的碰撞系数
     # 判断碰到左侧上壁
     for i in range(ballnum):
         if b1[i].r[0] + b1[i].r[1] < -sx + 1.414 * radius and b1[i].r[0] < -sx + radius / 1.414 and \
                 b1[i].r[1] > radius / 1.414:
+            en = np.array([1/1.414, 1/1.414])
+            ef = getvertical_vector(en)
             xf = 1.414 * radius - sx - b1[i].r[1]
             yf = 1.414 * radius - sx - b1[i].r[0]
             b1[i].r[0] = xf
             b1[i].r[1] = yf
-            vxf = -b1[i].v[1]
-            vyf = -b1[i].v[0]
-            b1[i].v[0] = funnelfactor * vxf
-            b1[i].v[1] = funnelfactor * vyf
+            vf1 = ef * np.dot(b1[i].v, ef)
+            vn1 = en * np.dot(b1[i].v, en)
+            b1[i].v = vf1 - funnelfactor * vn1
+
         # 判断碰到右侧上壁
         elif b1[i].r[1] - b1[i].r[0] < -sx + 1.414 * radius and b1[i].r[0] > sx - radius / 1.414 and \
                 b1[i].r[1] > radius / 1.414:
+            en = np.array([-1 / 1.414, 1 / 1.414])
+            ef = getvertical_vector(en)
             xf = -1.414 * radius + sx + b1[i].r[1]
             yf = 1.414 * radius - sx + b1[i].r[0]
             b1[i].r[0] = xf
             b1[i].r[1] = yf
-            vxf = b1[i].v[1]
-            vyf = b1[i].v[0]
-            b1[i].v[0] = funnelfactor * vxf
-            b1[i].v[1] = funnelfactor * vyf
+            vf1 = ef * np.dot(b1[i].v, ef)
+            vn1 = en * np.dot(b1[i].v, en)
+            b1[i].v = vf1 - funnelfactor * vn1
         # 判断碰到左侧下壁
         elif b1[i].r[1] - b1[i].r[0] > sx - 1.414 * radius and b1[i].r[0] < -sx + radius / 1.414 and \
                 b1[i].r[1] < -radius / 1.414:
+            en = np.array([1 / 1.414, -1 / 1.414])
+            ef = getvertical_vector(en)
             xf = 1.414 * radius - sx + b1[i].r[1]
             yf = -1.414 * radius + sx + b1[i].r[0]
             b1[i].r[0] = xf
             b1[i].r[1] = yf
-            vxf = b1[i].v[1]
-            vyf = b1[i].v[0]
-            b1[i].v[0] = funnelfactor * vxf
-            b1[i].v[1] = funnelfactor * vyf
+            vf1 = ef * np.dot(b1[i].v, ef)
+            vn1 = en * np.dot(b1[i].v, en)
+            b1[i].v = vf1 - funnelfactor * vn1
+
         # 判断碰到右侧下壁
         elif b1[i].r[0] + b1[i].r[1] > sx - 1.414 * radius and b1[i].r[0] > sx - radius / 1.414 and \
                 b1[i].r[1] < -radius / 1.414:
+            en = np.array([-1 / 1.414, -1 / 1.414])
+            ef = getvertical_vector(en)
             xf = -1.414 * radius + sx - b1[i].r[1]
             yf = -1.414 * radius + sx - b1[i].r[0]
             b1[i].r[0] = xf
             b1[i].r[1] = yf
-            vxf = -b1[i].v[1]
-            vyf = -b1[i].v[0]
-            b1[i].v[0] = funnelfactor * vxf
-            b1[i].v[1] = funnelfactor * vyf
+            vf1 = ef * np.dot(b1[i].v, ef)
+            vn1 = en * np.dot(b1[i].v, en)
+            b1[i].v = vf1 - funnelfactor * vn1
     return b1
 
 
 # 定义碰撞钉子
 def bouncenail(b1, n1):
+    railfactor = 0.8  # 与钉子碰撞的碰撞系数
     en = (b1.r - n1.r) / (np.linalg.norm(b1.r - n1.r))
     ef = getvertical_vector(en)
     vf1 = ef * np.dot(b1.v, ef)
     vn1 = -en * np.dot(b1.v, en)
     b1.v = vf1 + railfactor * vn1
-    b1.r = n1.r + en * (radius + rnail)
+    b1.r = n1.r + en * (Ball.radius + Nail.rnail)
     return b1
 
 
 # 小球与下方隔板的碰撞
 def bouncepartip(ball):
     b1 = ball
+    radius = Ball.radius
+    parfactor = 0.5  # 与下方隔板的碰撞系数
     for i in range(ballnum):
         for j in range(cn):
             if dx * j - dx * (cn + 1) / 2 < b1[i].r[0] < dx * j - dx * (cn - 1) / 2 < b1[i].r[0] + radius and \
-                    b1[i].r[1] < -dy * (cn - 1) - 2:
+                    b1[i].r[1] < -dy * (cn - 1) - 2 + radius:
                 b1[i].r[0] = dx * j - dx * (cn - 1) / 2 - radius
                 b1[i].v[0] = -b1[i].v[0] * parfactor
             elif dx * j - dx * (cn - 1) / 2 > b1[i].r[0] > dx * j - dx * (cn + 1) / 2 > b1[i].r[0] - radius and \
-                    b1[i].r[1] < -dy * (cn - 1) - 2:
+                    b1[i].r[1] < -dy * (cn - 1) - 2 + radius:
                 b1[i].r[0] = radius + dx * j - dx * (cn + 1) / 2
                 b1[i].v[0] = -b1[i].v[0] * parfactor
     return b1
@@ -155,6 +150,7 @@ def bouncepartip(ball):
 
 # 定义互相碰撞
 def bounceeachother(b1, b2):
+    k1 = 0.98  # 两小球碰撞因子
     en = (b1.r - b2.r) / (np.linalg.norm(b1.r - b2.r))
     ef = getvertical_vector(en)
     vf1 = ef * np.dot(b1.v, ef)
@@ -189,14 +185,27 @@ def update(index):
     return balls
 
 
+ballnum = 100  # 小球数量
+# 设置xy轴范围
+xmin = -32
+ymin = -44
+ymax = 32
+xmax = 32
+# 定义高尔顿板钉子的参数
+dx = 4.8  # 横向间距
+dy = 2.5  # 纵向间距
+cn = 14  # 层数
+# 定义侧板位置
+sx, sy = 4, 34
+nailnum = int(cn * (cn + 1) / 2)  # 钉子总数
 ball = []
 nail = []
 
 for i in range(ballnum):
-    xi = random.uniform(xmin + radius, xmax - radius)
-    yi = random.uniform(10, ymax - radius)
+    xi = random.uniform(xmin + Ball.radius, xmax - Ball.radius)
+    yi = random.uniform(10, ymax - Ball.radius)
     vxi = random.uniform(0, 0)
-    vyi = random.uniform(0, 0)
+    vyi = random.uniform(0, -10)
     r = [xi, yi]
     v = [vxi, vyi]
     ball.append(Ball(r, v))
